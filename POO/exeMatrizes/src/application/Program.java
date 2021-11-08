d4f09747c076283791948b377b0cc1e17286279b
Virginia Lucia Silva <vlucisil@everis.com>
2021-11-08 10:25:49 -0300

    Matrizes

--- POO/exeMatrizes/src/application/Program.java
+++ POO/exeMatrizes/src/application/Program.java
@@ -14,22 +14,22 @@ public class Program {
         int n = sc.nextInt();
         int [][] mat = new int [n][n]; //os dois colchetes indicam que é um arranjo bidimensional.
 
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat[i].length; j++){
                 mat [i][j] = sc.nextInt();
             }
         }
 
         System.out.println("Main Diagonal: ");
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < mat.length; i++) {
             System.out.printf(mat[i][i] + " ");
         }
 
         System.out.println();
 
         int count = 0;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
+        for (int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat[i].length; j++){
                 if (mat [i][j] < 0) {
                     count ++;
                 }
