919eb6f69d6bd3ea675b0b0b6613684a16b6a64d
Virginia Lucia Silva <vlucisil@everis.com>
2021-11-16 13:49:54 -0300

    exe composicao

---
+++ POO/enum/src/entities/enuns/OrderStatus.java
@@ -0,0 +1,10 @@
+package entities.enuns;
+
+public enum OrderStatus {
+
+    PENDING_PAYMENT, //AGUARDANDO PGTO
+    PROCESSING, //PROCESSANDO
+    SHIPPED, //ENVIADO
+    DELIVERED; //ENTREGUE
+
+}
