e513d7d966282eadf4938b875b102d41d5e5a9a2
Virginia Silva <virginia_hol@hotmail.com>
2022-01-20 16:27:23 -0300

    arquivos

---
+++ File e Scanner/src/application/Program.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package application;
+
+/**
+ * @author vlucisil
+ *
+ */
+public class Program {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
