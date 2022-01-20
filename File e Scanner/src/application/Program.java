91f685f095112e59e7ddfaa541c83d642454e18d
Virginia Silva <virginia_hol@hotmail.com>
2022-01-20 16:54:12 -0300

    teste

--- File e Scanner/src/application/Program.java
+++ File e Scanner/src/application/Program.java
@@ -3,18 +3,33 @@
  */
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * @author vlucisil
  *
  */
 public class Program {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		File file = new File("C:\\Desenvolvimento\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch (IOException e) {
+			System.out.println("error" + e.getMessage());
+		}
+		
+		finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
 	}
-
 }
---
+++ FileBufferedReader/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+/**
+ * @author vlucisil
+ *
+ */
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String path = "C:\\Desenvolvimento\\in.txt";
+
+	}
+
+}
